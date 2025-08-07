d93b2e63820a7703c5770129a995adcf11494134
Rafaela Rodrigues <98193010+Lumecloud@users.noreply.github.com>
2025-08-06 21:49:12 -0300

    Add files via upload

--- menu.php
+++ menu.php
@@ -6,6 +6,9 @@
     <title>Exercicio 6 </title>
 </head>
 <body>
+
+    <?php include "atual.php"?>
+
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Barra</a>
@@ -13,16 +16,15 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        include('menu.php');
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link al<?php if ($pg_atu == 'home') { echo 'active'; } ?>" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="atual.php">Produtos</a>
+          <a class="nav-link <?php if ($pg_atual == 'produtos') { echo 'active'; } ?>" href="produtos.php">Produtos</a>
         </li>
          <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="#">Carrinho</a>
+          <a class="nav-link <?php if ($pg_atual == 'carrinho') { echo 'active'; } ?>" href="carrinho.php">Carrinho</a>
         </li>
       </ul>
     </div>
